c327f19008e35eb445b6c7e36715710d6a016bdb
Asaduzzaman_mac <samrat.asad@gmail.com>
2024-08-01 14:27:45 +0600

    all CRUD function for expense database

--- lib/database/expense_database.dart
+++ lib/database/expense_database.dart
@@ -21,17 +21,59 @@ class ExpenseDatabase extends ChangeNotifier {
   }
 
 /*
-
   G E T T E R
-
-  */
+ */
   List<Expense> get allExpense => _allExpenses;
 
 /*
-
-  O P E R A T I O N
-
+ O P E R A T I O N
   */
+
+//create -  a new expanse
+  Future<void> createNewExpense(Expense newExpense) async {
+    //add to db
+    await isar.writeTxn(() => isar.expenses.put(newExpense));
+
+    //re-read from db
+    await readExpense();
+  }
+
+//read - expense from the database
+  Future<void> readExpense() async {
+    //fetch all existing expense from db
+    List<Expense> fetchExpenses = await isar.expenses.where().findAll();
+
+    //give to local expense list
+    _allExpenses.clear();
+    _allExpenses.addAll(fetchExpenses);
+
+    //update ui
+    notifyListeners();
+  }
+
+//update - edite an expense in db
+
+  Future<void> updateExpense(int id, Expense updatedExpense) async {
+    //make sure new expense has same id as existing one
+    updatedExpense.id = id;
+
+    //update in db
+    await isar.writeTxn(() => isar.expenses.put(updatedExpense));
+
+    //re-read from db
+    await readExpense();
+  }
+
+//delete - an expense
+
+  Future<void> deleteExpense(int id) async {
+    //delete from db
+    await isar.writeTxn(() => isar.expenses.delete(id));
+
+    //re-read from db
+    await readExpense();
+  }
+
 /*
 
   H E L P E R
